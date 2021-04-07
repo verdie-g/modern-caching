c5e16e8f58ac73fe7e69cecb15f7aaf5f8ffa09c
verdie-g <gregoire.verdier@gmail.com>
2021-04-07 19:15:27 +0200

    Document ICache

--- src/ModernCaching/LocalCaching/CacheEntry.cs
+++ src/ModernCaching/LocalCaching/CacheEntry.cs
@@ -2,5 +2,10 @@
 
 namespace ModernCaching.LocalCaching
 {
+    /// <summary>
+    /// Entry of an <see cref="ICache{TKey,TValue}"/>.
+    /// </summary>
+    /// <param name="Value">The value of the entry.</param>
+    /// <param name="ExpirationTime">The UTC time after which the value is considered stale</param>
     public record CacheEntry<TValue>(TValue Value, DateTime ExpirationTime);
 }
--- src/ModernCaching/LocalCaching/ICache.cs
+++ src/ModernCaching/LocalCaching/ICache.cs
@@ -2,10 +2,30 @@
 
 namespace ModernCaching.LocalCaching
 {
+    /// <summary>
+    /// Cache that gets its key synchronously. Typically an in-memory cache.
+    /// </summary>
+    /// <typeparam name="TKey"></typeparam>
+    /// <typeparam name="TValue"></typeparam>
     public interface ICache<in TKey, TValue>
     {
-        // Should never throw.
+        /// <summary>
+        /// Gets the value associated with the specified key.
+        /// </summary>
+        /// <param name="key">The key of the element to get.</param>
+        /// <param name="value">Value associated with the specified key, if the key is found; otherwise, the default
+        /// value for the type of the value parameter. Existing value should be returned even if stale. <see cref="CacheEntry{TValue}.Value"/>
+        /// can be null if the data source returns null.</param>
+        /// <returns>True if the cache contains an element with the specified key; otherwise, false.</returns>
+        /// <remarks>This method should never throw.</remarks>
         bool TryGet(TKey key, [MaybeNullWhen(false)] out CacheEntry<TValue> value);
+
+        /// <summary>
+        /// Sets the value associated with the specified key.
+        /// </summary>
+        /// <param name="key">The key of the element to set.</param>
+        /// <param name="value">The value associated with the specified key.</param>
+        /// <remarks>This method should never throw.</remarks>
         void Set(TKey key, CacheEntry<TValue> value);
     }
 }
