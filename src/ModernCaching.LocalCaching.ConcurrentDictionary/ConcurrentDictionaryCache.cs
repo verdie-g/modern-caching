bee493ef68993c31b05f569a2b361361f13cb2a7
verdie-g <gregoire.verdier@gmail.com>
2021-05-28 01:21:01 +0200

    Add ICache implementation with a ConcurrentDictionary

---
+++ src/ModernCaching.LocalCaching.ConcurrentDictionary/ConcurrentDictionaryCache.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Concurrent;
+
+namespace ModernCaching.LocalCaching.ConcurrentDictionary
+{
+    /// <summary>
+    /// Implementation of a <see cref="ICache{TKey,TValue}"/> using a <see cref="ConcurrentDictionary{TKey,TValue}"/>.
+    /// The entries in this cache are never evicted. It can be a good thing to avoid gen 2 fragmentation.
+    /// </summary>
+    /// <typeparam name="TKey">The type of the keys.</typeparam>
+    /// <typeparam name="TValue">The type of the values.</typeparam>
+    public class ConcurrentDictionaryCache<TKey, TValue> : ICache<TKey, TValue>
+    {
+        private readonly ConcurrentDictionary<TKey, CacheEntry<TValue>> _dictionary;
+
+        public ConcurrentDictionaryCache() => _dictionary = new ConcurrentDictionary<TKey, CacheEntry<TValue>>();
+        public bool TryGet(TKey key, out CacheEntry<TValue> entry) => _dictionary.TryGetValue(key, out entry);
+        public void Set(TKey key, CacheEntry<TValue> entry) => _dictionary[key] = entry;
+    }
+}
