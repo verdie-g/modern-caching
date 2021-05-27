a8a045a4a6f74e825bd8d800575762e4c44bac22
verdie-g <gregoire.verdier@gmail.com>
2021-05-28 00:08:54 +0200

    Target .NET Standard 2.1

---
+++ src/ModernCaching/Utils/IsExternalInit.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel;
+
+/* Required to use records when targeting .NET Standard 2.1 (https://stackoverflow.com/a/62656145/5407910) */
+namespace System.Runtime.CompilerServices
+{
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public class IsExternalInit{}
+}
