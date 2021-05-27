dcde999bd4133028ea241583b4b9290f1933d77a
verdie-g <gregoire.verdier@gmail.com>
2021-05-27 23:59:21 +0200

    Add tests for Dispose

---
+++ src/ModernCaching.UTest/ReadOnlyCacheTest.Dispose.cs
@@ -0,0 +1,15 @@
+﻿using ModernCaching.Utils;
+using Moq;
+using NUnit.Framework;
+
+namespace ModernCaching.UTest
+{
+    public class ReadOnlyCacheTest_Dispose
+    {
+        public void DisposeShouldNotThrow()
+        {
+            ReadOnlyCache<int, int> cache = new(null, null, null!, Mock.Of<ITimer>());
+            Assert.DoesNotThrow(() => cache.Dispose());
+        }
+    }
+}
