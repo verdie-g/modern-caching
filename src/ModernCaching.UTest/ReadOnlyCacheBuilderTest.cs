d92847ceaf47a9d39b7a90664a60645884064d9f
verdie-g <gregoire.verdier@gmail.com>
2021-05-30 15:41:23 +0200

    Add ReadOnlyCacheBuilder tests

---
+++ src/ModernCaching.UTest/ReadOnlyCacheBuilderTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using ModernCaching.DataSource;
+using ModernCaching.DistributedCaching;
+using ModernCaching.LocalCaching;
+using Moq;
+using NUnit.Framework;
+
+namespace ModernCaching.UTest
+{
+    public class ReadOnlyCacheBuilderTest
+    {
+        [Test]
+        public void FullBuilderShouldNotThrow()
+        {
+            Assert.DoesNotThrowAsync(() =>
+                new ReadOnlyCacheBuilder<string, IPEndPoint>("test", Mock.Of<IDataSource<string, IPEndPoint>>())
+                    .WithLocalCache(Mock.Of<ICache<string, IPEndPoint>>())
+                    .WithDistributedCache(Mock.Of<IAsyncCache>(), Mock.Of<IKeyValueSerializer<string, IPEndPoint>>())
+                    .BuildAsync()
+            );
+        }
+    }
+}
