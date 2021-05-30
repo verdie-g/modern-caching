957ef9343942f7d9355b4b20de6fe89da8fde0ef
verdie-g <gregoire.verdier@gmail.com>
2021-05-30 15:00:12 +0200

    Make AsyncCacheResult a struct

--- src/ModernCaching/DistributedCaching/AsyncCacheResult.cs
+++ src/ModernCaching/DistributedCaching/AsyncCacheResult.cs
@@ -3,8 +3,28 @@
     /// <summary>
     /// Result of the loading of a key from an <see cref="IAsyncCache"/>.
     /// </summary>
-    /// <param name="Status">The status of the result.</param>
-    /// <param name="Value">The value associated with the key. Null if status is <see cref="AsyncCacheStatus.Miss"/> or
-    /// <see cref="AsyncCacheStatus.Error"/>.</param>
-    public record AsyncCacheResult(AsyncCacheStatus Status, byte[]? Value);
+    public readonly struct AsyncCacheResult
+    {
+        /// <summary>
+        /// The status of the result.
+        /// </summary>
+        public readonly AsyncCacheStatus Status;
+
+        /// <summary>
+        /// The value associated with the key. Null if status is <see cref="AsyncCacheStatus.Miss"/> or <see cref="AsyncCacheStatus.Error"/>.
+        /// </summary>
+        public readonly byte[]? Value;
+
+        public AsyncCacheResult(AsyncCacheStatus status, byte[]? value)
+        {
+            Status = status;
+            Value = value;
+        }
+
+        public void Deconstruct(out AsyncCacheStatus status, out byte[]? value)
+        {
+            status = Status;
+            value = Value;
+        }
+    }
 }
