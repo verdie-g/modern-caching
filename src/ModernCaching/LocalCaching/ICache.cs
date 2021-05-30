d86e4434e53bf7cbd884b05234727efd8efbbd0b
verdie-g <gregoire.verdier@gmail.com>
2021-05-30 15:11:10 +0200

    Complete ICache documentation

--- src/ModernCaching/LocalCaching/ICache.cs
+++ src/ModernCaching/LocalCaching/ICache.cs
@@ -5,8 +5,8 @@ namespace ModernCaching.LocalCaching
     /// <summary>
     /// Cache that gets its key synchronously. Typically an in-memory cache.
     /// </summary>
-    /// <typeparam name="TKey"></typeparam>
-    /// <typeparam name="TValue"></typeparam>
+    /// <typeparam name="TKey">The type of the keys in the cache.</typeparam>
+    /// <typeparam name="TValue">The type of the values in the cache entries.</typeparam>
     public interface ICache<in TKey, TValue>
     {
         /// <summary>
@@ -15,7 +15,9 @@ namespace ModernCaching.LocalCaching
         /// <param name="key">The key of the element to get.</param>
         /// <param name="entry">Entry associated with the specified key, if the key is found; otherwise, the value is
         /// set to the default for the type of the value parameter. Existing entries should be returned even if stale.
-        /// <see cref="CacheEntry{TValue}.Value"/> can be null if the data source returns null.</param>
+        /// If the cache is evicting, the implementation should have a grace period during which stale entries are kept
+        /// in memory so that <see cref="IReadOnlyCache{TKey,TValue}.TryPeek"/> can return a stale value during its
+        /// refresh. <see cref="CacheEntry{TValue}.Value"/> can be null if the data source returns null.</param>
         /// <returns>True if the cache contains an entry with the specified key; otherwise, false.</returns>
         /// <remarks>This method should never throw.</remarks>
         bool TryGet(TKey key, [MaybeNullWhen(false)] out CacheEntry<TValue?> entry);
