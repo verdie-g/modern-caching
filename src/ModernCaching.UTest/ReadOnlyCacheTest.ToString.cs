44d1930084ca6cd7aa0476af67b1df4653d777d1
verdie-g <gregoire.verdier@gmail.com>
2021-08-27 23:48:32 +0200

    Add ReadOnlyCache.ToString test

---
+++ src/ModernCaching.UTest/ReadOnlyCacheTest.ToString.cs
@@ -0,0 +1,19 @@
+﻿using ModernCaching.Utils;
+using Moq;
+using NUnit.Framework;
+
+namespace ModernCaching.UTest
+{
+    public class ReadOnlyCacheTest_ToString
+    {
+        private const string C = "cache_test";
+
+        [Test]
+        public void ToStringShouldReturnCacheName()
+        {
+            ReadOnlyCache<int, int> cache = new(C, null, null, null!, new ReadOnlyCacheOptions(), Mock.Of<ITimer>(),
+                Mock.Of<IDateTime>(), Mock.Of<IRandom>());
+            Assert.AreEqual(C, cache.ToString());
+        }
+    }
+}
